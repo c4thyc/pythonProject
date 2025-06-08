43259b7f7c6346da46eac2ee8eb6f357372fb1ed
c4thyc <cathyc22@nycstudents.net>
2025-06-08 17:06:05 -0400

    redid everything, still need to organize the functions

--- mainn.py
+++ mainn.py
@@ -5,8 +5,6 @@ from tkinter import ttk
 from dataHistory import save_user_info, load_user_info, save_monthly_data, load_monthly_data
 from recommendation import recommendations_frame
 
-#
-
 class GUI:
 
     # initializing frame
@@ -21,97 +19,158 @@ class GUI:
 
         # list of all the frames (saves space)
         self.frames = {}
-        for frame in (NameFrame, GoalFrame, MenuFrame, BudgetFrame, HistoryFrame, RecFrame):
-            window = frame(self.window, self)
-            window.grid(row = 0, column = 0, sticky = "nsew")
-            self.frames[frame] = window
+
+        for f in (NameFrame, GoalFrame, MenuFrame, BudgetFrame, HistoryFrame, RecFrame):
+            frame = f(self.window, self)
+            self.frames[f] = frame
+            frame.grid(row = 0, column = 0, sticky = "nsew")
+
+        # loads existing user info (name, goal) if applicable
+        self.userData = load_user_info()
+
+        # begins with the name frame
+        self.show_frame(NameFrame)
 
     # switches between frames
     def show_frame(self, frame):
-        intendedFrame = self.frames[frame]
-        intendedFrame.tkraise()
+
+        self.frames[frame].tkraise()
 
     # personalizes experience, keeps goal
     def personalize(self):
-        name = self.nameEntry.get().strip()
+
+        name_frame = self.frames[NameFrame]
+        name = name_frame.nameEntry.get().strip()
+
         if name:
             if self.userData is None:
                 self.userData = {}
             self.userData = load_user_info() or {}
             self.userData["name"] = name
-            self.welcomeLabel.config(text = f"Welcome, {name}!")
-            self.new_frame("goalFrame")
+            self.frames[GoalFrame].welcomeLabel.config(text = f"Welcome, {name}!")
+            self.show_frame(GoalFrame)
 
     # puts goal on the screen as a reminder
     def goal_reminder(self):
-        goal = self.goalEntry.get().strip()
+
+        goal_frame = self.frames[GoalFrame]
+        goal = goal_frame.goalEntry.get().strip()
 
         if self.userData is None:
             self.userData = {}
 
         self.userData["goal"] = goal
         save_user_info(self.userData, goal)
-        self.goalReminder.config(text=f"Goal: {goal}")
-        self.frames["budgetingFrame"].children[self.goalReminder.winfo_name()].config(text=f"Goal: {goal}")
-        self.new_frame("menuFrame")
+        self.frames[BudgetFrame].goalReminder.config(text = f"Goal: {goal}")
+        self.frames[BudgetFrame].children[goal_frame.goalReminder.winfo_name()].config(text = f"Goal: {goal}")
+        self.show_frame(MenuFrame)
+
+        # links the data to the month and stores it
+    def submit_month(self):
+
+        month_frame = self.frames[MenuFrame]
+        month = month_frame.monthEntry.get().strip()
+
+        if not month:
+            return
+
+        # list for all the data for that month
+        data = {
+            "income": float(month_frame.incomeEntry.get()),
+            "savings": float(month_frame.savingsEntry.get()),
+            "utilities": float(month_frame.utilEntry.get()),
+            "transport": float(month_frame.transportEntry.get())
+            }
+
+        save_monthly_data(month, data)  # saves it before it gets deleted
+        month_frame.monthEntry.delete(0, 'end')  # removes the data so new data can be entered
+        month_frame.incomeEntry.delete(0, 'end')
+        month_frame.savingsEntry.delete(0, 'end')
+        month_frame.utilEntry.delete(0, 'end')
+        month_frame.transportEntry.delete(0, 'end')
 
     def to_recommendations(self):
-        self.new_frame("recommendationsFrame")
+
+        self.show_frame(RecFrame)
+
+    def display_history(self, controller):
+
+        history_frame = self.frames[HistoryFrame]
+        history_frame.historyText.delete('1.0', tk.END)
+        data = load_monthly_data()
+
+        if not data:
+            history_frame.historyText.insert(tk.END, "No budgeting data found.\n")
+
+        else:
+            for month, details in data.items():
+                history_frame.historyText.insert(tk.END, f"{month}:\n")
+                for key, value in details.items():
+                    history_frame.historyText.insert(tk.END, f"  {key}: ${value}\n")
+                history_frame.historyText.insert(tk.END, "\n")
+        controller.show_frame(HistoryFrame)
 
 # frame where user enters name
 class NameFrame(tk.Frame):
 
     def __init__(self, master, controller):
+
         tk.Frame.__init__(self, master)
 
         # title of frame
         ttk.Label(master = self, text = "Smart Budgeter", font = ("Times New Roman", 30)).pack(padx = 210, pady = 30)
         # asks user to enter name, stores it in a variable
-        self.nameEntry = ttk.Label(master = self, text = "Enter your name", font = ("Times New Roman", 14))
-        self.nameEntry.pack(pady = 10)
+        ttk.Label(master = self, text = "Enter your name", font = ("Times New Roman", 14)).pack(pady = 10)
         # name box
-        ttk.Entry(master = self, font = ("Times New Roman", 10), width = 40).pack(pady = 10)
+        self.nameEntry = ttk.Entry(master = self, font = ("Times New Roman", 10), width = 30)
+        self.nameEntry.pack(pady = 10)
         # button to go next
-        tk.Button(master = self, font = ("Times New Roman", 10), text = "Enter", command = lambda: controller.show_frame(GoalFrame))
+        ttk.Button(master = self, text = "Enter", command = controller.personalize).pack(pady = 5)
 
 # frame where user enters goal, personalization
 class GoalFrame(tk.Frame):
 
     def __init__(self, master, controller):
+
         tk.Frame.__init__(self, master)
 
         # title of frame
-        ttk.Label(master = self, text = "", font = ("Times New Roman", 30)).pack(pady = 20)
+        self.welcomeLabel = ttk.Label(master = self, text = "", font = ("Times New Roman", 30))
+        self.welcomeLabel.pack(pady = 20)
         # question about goal, stores goal into variable
-        self.goalEntry = ttk.Label(master = self, text = "What is your budgeting goal?", font = ("Arial", 20))
-        self.goalEntry.pack(pady = 5)
+        ttk.Label(master = self, text = "What is your budgeting goal?", font = ("Times New Roman", 20)).pack(pady = 5)
         # entry box for the goal
-        ttk.Entry(master = self, font = ("Times New Roman", 20)).pack(pady = 20)
+        self.goalEntry = ttk.Entry(master = self, font = ("Times New Roman", 14), width = 50)
+        self.goalEntry.pack(pady = 20)
         # button to go next to budgeting function
-        ttk.Button(master = self, text = "Enter", command = lambda: controller.show_frame(MenuFrame)).pack(pady = 15)
+        ttk.Button(master = self, text = "Enter", command = controller.goal_reminder).pack(pady = 15)
 
 # frame where user can navigate to other frames
 class MenuFrame(tk.Frame):
+
     def __init__(self, master, controller):
+
         tk.Frame.__init__(self, master)
 
         # title of menu
         ttk.Label(master = self, text = "Main Menu", font = ("Times New Roman", 24)).pack(pady = 20)
         # budgeting tool
-        ttk.Button(master = self, text = "Monthly Budget", command = lambda: controller.show_Frame(BudgetFrame)).pack(pady = 10)
+        ttk.Button(master = self, text = "Monthly Budget", command = lambda: controller.show_frame(BudgetFrame)).pack(pady = 10)
         # budgeting history
-        ttk.Button(master = self, text = "Budget History", command = lambda: controller.display_history).pack(pady = 10)
+        ttk.Button(master = self, text = "Budget History", command = controller.display_history).pack(pady = 10)
         # recommendations
-        ttk.Button(master = self, text = "Recommendations", command = lambda: controller.to_recommendations).pack(pady = 10)
+        ttk.Button(master = self, text = "Recommendations", command = controller.to_recommendations).pack(pady = 10)
 
 # frame where user budgets at a monthly rate
 class BudgetFrame(tk.Frame):
+
     def __init__(self, master, controller):
+
         tk.Frame.__init__(self, master)
 
         # puts goal on top for reminder
-        ttk.Label(master = self, text = "", font = ("Times New Roman", 14)).pack(pady = 10)
-
+        self.goalReminder = ttk.Label(master = self, text = "", font = ("Times New Roman", 14))
+        self.goalReminder.pack(pady = 10)
         # to enter each month
         ttk.Label(master = self, text = "Month:").pack()
         self.monthEntry = ttk.Entry(master = self)
@@ -147,50 +206,22 @@ class BudgetFrame(tk.Frame):
         # button for going back to the main menu
         ttk.Button(master = self, text = "Back to Main Menu", command = lambda: controller.show_frame(MenuFrame)).pack(pady = 10)
 
-    #links the data to the month and stores it
-    def submit_month(self):
-        month = self.monthEntry.get().strip()
-        if not month:
-            return
-        #list for all the data for that month
-        data = {
-            'income': float(self.incomeEntry.get()),
-            'savings': float(self.savingsEntry.get()),
-            'utilities': float(self.utilEntry.get()),
-            'transport': float(self.transportEntry.get())
-        }
-        save_monthly_data(month, data)  #saves it before it gets deleted
-        self.monthEntry.delete(0, 'end')  #removes the data so new data can be entered
-        self.incomeEntry.delete(0, 'end')
-        self.savingsEntry.delete(0, 'end')
-        self.utilEntry.delete(0, 'end')
-        self.transportEntry.delete(0, 'end')
-
 # frame where user can view their budgeting history if applicable
 class HistoryFrame(tk.Frame):
+
     def __init__(self, master, controller):
+
         tk.Frame.__init__(self, master)
 
         self.historyText = tk.Text(master = self, height = 20, width = 80)
         self.historyText.pack(pady = 10)
         ttk.Button(master = self, text = "Back to Main Menu", command = lambda: controller.show_frame(MenuFrame)).pack(pady = 10)
 
-    def display_history(self):
-        self.historyText.delete('1.0', tk.END)
-        data = load_monthly_data(self)
-        if not data:
-            self.historyText.insert(tk.END, "No budgeting data found.\n")
-        else:
-            for month, details in data.items():
-                self.historyText.insert(tk.END, f"{month}:\n")
-                for key, value in details.items():
-                    self.historyText.insert(tk.END, f"  {key}: ${value}\n")
-                self.historyText.insert(tk.END, "\n")
-        self.show_frame(HistoryFrame)
-
 # frame where user can view budgeting recommendations (50/30/20)
 class RecFrame(tk.Frame):
+
     def __init__(self, master, controller):
+
         tk.Frame.__init__(self, master)
 
         # displays the rec info
